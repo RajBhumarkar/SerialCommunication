af0e7369c6a63c00bfd10a4554f2dffebcc227ea
RajBhumarkar <rajbhumarkar07@gmail.com>
2025-04-09 16:57:54 +0530

    IMU and ROTARY PKG

---
+++ build/imu_pkg/build/lib/imu_pkg/Serial_node.py
@@ -0,0 +1,36 @@
+#!/ usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+import serial
+from std_msgs.msg import String  # or a custom message type
+import numpy as np
+class SerialNode(Node):
+    def __init__(self):
+        super().__init__('serial_node')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        if self.serial_port.in_waiting:
+            # line = self.serial_port.readline().decode('utf-8').rstrip()
+            line = self.serial_port.readline()
+            # print(f"Raw data: {line}")
+            # print(type(line))
+            # Process line and publish
+            msg = String()
+            # msg.data = line  # or process as needed
+            msg.data = line.decode('utf-8', errors='ignore').strip()
+
+            self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    serial_node = SerialNode()
+    rclpy.spin(serial_node)
+    serial_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ build/imu_pkg/build/lib/imu_pkg/Serial_node_imu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+import serial
+from std_msgs.msg import String  # or a custom message type
+
+class SerialNodeIMU(Node):
+    def __init__(self):
+        super().__init__('serial_node')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def timer_callback(self):
+        if self.serial_port.in_waiting:
+            line = self.serial_port.readline().decode('utf-8').rstrip()
+            # Process line and publish
+            msg = String()
+            msg.data = line  # or process as needed
+            self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    serial_node = SerialNodeIMU()
+    rclpy.spin(serial_node)
+    serial_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
